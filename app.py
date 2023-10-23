054806a5545a17c02b4feffc5e554259e6374c16
AliakseiDunets <aliaksei.dunets.public@gmail.com>
2023-10-24 01:23:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
 WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{BOT_TOKEN}'
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
